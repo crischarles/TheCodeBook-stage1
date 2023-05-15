27cbb4f825955efeffafbf1bfb6aee49b5c5419a
Crischarles <crischarles.ad03@gmail.com>
2023-05-15 10:17:56 -0300

    update decscript

--- decscript.sh
+++ decscript.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #
-############################################################################
-#created by Crischarles D. Arruda
-#
-#This apply for Single Substitution Monoalphabetic Cipher for Pt-Br text
-#
-#
+############################################################
+#created by Crischarles D. Arruda			   #
+#							   #
+#This script can be applied only for:			   #
+# Single Substitution Monoalphabetic Cipher for Pt-Br text #
+#							   #
+############################################################
+
+
