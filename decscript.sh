54914d418a26b387b7d94e3b802d32630f9f33be
Crischarles <crischarles.ad03@gmail.com>
2023-05-21 10:40:47 -0300

    decscript version 1.1

--- decscript.sh
+++ decscript.sh
@@ -69,15 +69,62 @@ function decfunction(){
 	
 	decfilepath=/tmp/decfile.txt
 	cat "$1" | tr '[:lower:]' '[:upper:]' > $decfilepath
-	#decfile=$(cat "$1" | tr '[:lower:]' '[:upper:]')
 
+	#Version 1.0
 	for i in {0..25};
 	do
 		tr "${sabc[$i]}" "${ptbrfr[$i]}" < $decfilepath > /tmp/decfiletmp.txt && mv /tmp/decfiletmp.txt $decfilepath
 		#echo "${sabc[$i]}" "${ptbrfr[$i]}"
 	done
 	
-	#echo $decfile > $decfilepath
+	#Version 1.1
+	#Checking whether the two most frequent letters are in the correct position (A and E), using the word "que"
+	#"que" is the most frequent word by far with just 3 letters in PtBr
+	
+	wordslist=$(cat $decfilepath | tr '[:upper:]' '[:lower:]' | grep -o '\b[a-z]\{3\}\b' | sort | uniq -c | sort -nr)
+	worde=$(echo $wordslist | grep -Eo '\b\w+e\b' | head -n 1)
+	worda=$(echo $wordslist | grep -Eo '\b\w+a\b' | head -n 1)
+	
+	#Searching for the letter 'q'
+	cutworde=$(echo $worde | cut -c 1)
+	cutworda=$(echo $worda | cut -c 1)
+	echo "cutworda: " $cutworda
+	echo "cutworde: " $cutworde
+
+	countcutworda=$(cat $decfilepath | grep -Eo "\b\w+${cutworda}\b" | wc -l)
+	countcutworde=$(cat $decfilepath | grep -Eo "\b\w+${cutworde}\b" | wc -l)
+	
+	if [ $countcutworda -gt $countcutworde ]
+	then
+		echo $cutworde " is the letter q"
+		wordque=$worde
+	else
+		echo $cutworda " is the letter q"
+		wordque=$worda
+	fi
+
+	#################
+	#Letter replacement time based on the word "que"
+	wordque1=$(echo $wordque | cut -c 1)
+	wordque2=$(echo $wordque | cut -c 2)
+	wordque3=$(echo $wordque | cut -c 3)
+	
+	echo "que1: $wordque1"
+	echo "que2: $wordque2"
+	echo "que3: $wordque3"
+	
+	tr "q" "+" < $decfilepath > /tmp/decfiletmp.txt && mv /tmp/decfiletmp.txt $decfilepath
+	tr "${wordque1}" "q" < $decfilepath > /tmp/decfiletmp.txt && mv /tmp/decfiletmp.txt $decfilepath
+	tr "+" "${wordque1}" < $decfilepath > /tmp/decfiletmp.txt && mv /tmp/decfiletmp.txt $decfilepath
+
+	tr "u" "+" < $decfilepath > /tmp/decfiletmp.txt && mv /tmp/decfiletmp.txt $decfilepath
+        tr "${wordque2}" "u" < $decfilepath > /tmp/decfiletmp.txt && mv /tmp/decfiletmp.txt $decfilepath
+        tr "+" "${wordque2}" < $decfilepath > /tmp/decfiletmp.txt && mv /tmp/decfiletmp.txt $decfilepath
+
+        tr "e" "+" < $decfilepath > /tmp/decfiletmp.txt && mv /tmp/decfiletmp.txt $decfilepath
+        tr "${wordque3}" "e" < $decfilepath > /tmp/decfiletmp.txt && mv /tmp/decfiletmp.txt $decfilepath
+        tr "+" "${wordque3}" < $decfilepath > /tmp/decfiletmp.txt && mv /tmp/decfiletmp.txt $decfilepath
+
 }
 
 
@@ -97,4 +144,6 @@ decfunction $1
 echo "sfilefr ${sfilefr[*]}"
 echo "sabc ${sabc[*]}"
 cat /tmp/decfile.txt
+
+
 
